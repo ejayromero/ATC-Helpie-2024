066eddabe27fe677f81aa615f7def0649984c4a8
Jade Therras <jade.therras@epfl.ch>
2024-04-07 23:51:45 +0200

    helpie v1

---
+++ Helpie2/app/src/main/java/com/example/helpie/ui/theme/Color.kt
@@ -0,0 +1,43 @@
+package com.example.helpie.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//couleur de base
+val md_theme_light_primary = Color(0xFF6A5F00)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xff0099ff)
+val md_theme_light_onPrimaryContainer = Color(0xFFFFFFFF)
+
+//couleur bouton prioritaire
+val md_theme_light_secondary = Color(0xFF645F41)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFBA1A1A)
+val md_theme_light_onSecondaryContainer = Color(0xFFFFFFFF)
+
+//couleur bouton aide/suite
+val md_theme_light_tertiary = Color(0xFF416651)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFF1D1C16)
+val md_theme_light_onTertiaryContainer = Color(0xFFFFFFFF)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF1D1C16)
+
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF1D1C16)
+
+val md_theme_light_surfaceVariant = Color(0xFFE8E2D0)
+val md_theme_light_onSurfaceVariant = Color(0xFF4A4739)
+
+val md_theme_light_outline = Color(0xFF7B7768)
+val md_theme_light_inverseOnSurface = Color(0xFFF5F0E7)
+val md_theme_light_inverseSurface = Color(0xFF32302A)
+val md_theme_light_inversePrimary = Color(0xFFDCC830)
+val md_theme_light_surfaceTint = Color(0xFF6A5F00)
+val md_theme_light_outlineVariant = Color(0xFFCCC6B5)
+val md_theme_light_scrim = Color(0xFF000000)
