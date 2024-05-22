20bce119ccb387e52fcb358d55f74923ba93d511
Claire <claire.payoux@epfl.ch>
2024-05-22 10:15:11 +0200

    homogeneisation of buttons and text except in SummaryScreen

--- Helpie2/app/src/main/java/com/example/helpie/ui/SummaryScreen.kt
+++ Helpie2/app/src/main/java/com/example/helpie/ui/SummaryScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
